a5de39df9d03e3790b1729513606c87ffd8f0295
claudiaalmeidavs <claudiaalmeida.traducao@gmail.com>
2023-06-27 15:43:50 +0100

    Adds styling to the details page and country list pages

--- src/Components/CountryCard/CountryCard.jsx
+++ src/Components/CountryCard/CountryCard.jsx
@@ -3,20 +3,17 @@ import "./CountryCard.css";
 import { Link } from "react-router-dom";
 
 export default function CountryCard({ country }) {
-  const { ccn3, name, flags:{png}, capital, region, population, area } = country;
+  const { ccn3, name, flags:{png}, capital, region, population } = country;
 
   return country ? (
-    <Link to={`/countries/${ccn3}`}>
-      <div className="country-card-container">
+    <Link className="country-card-container" to={`/countries/${ccn3}`}>
         <h1 className="country-name">{name.common}</h1>
         <img className="card-image" src={png} alt={name.common}/>
         <div className="country-info">
           <p className="card-info"><strong>Capital</strong>: {capital}</p>
           <p className="card-info"><strong>Region</strong>: {region}</p>
           <p className="card-info"><strong>Population</strong>: {population}</p>
-          <p className="card-info"><strong>Area</strong>: {area} km2</p>
         </div>
-      </div>
     </Link>
   ) : null;
 }
--- src/Pages/CountryDetails/CountryDetails.jsx
+++ src/Pages/CountryDetails/CountryDetails.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import GoogleMapReact from "google-map-react";
+import "./CountryDetails.css";
 
 export default function CountryDetails() {
   const { id } = useParams();
@@ -20,21 +20,22 @@ export default function CountryDetails() {
     fetchCountry();
   }, []);
 
+
   return (
     country ? 
     (<div className="country-details-container">
-      <h3>{country.name.common}</h3>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: '' }}
-        defaultCenter={country.maps.googleMaps}
-      />
-      <p><strong>Area</strong>: {country.area} km2</p>
-      <p><strong>Capital</strong>: {country.capital}</p>
-      <p><strong>Continent</strong>: {country.continents}</p>
-      <p><strong>Languages</strong>:</p>
+      <h2 className="details-title">{country.name.common}</h2>
+      <img className="details-image" src={country.flags.png} alt={country.name.common}/>
+      {country.unMember = true ? <p className="un-member-p">UN Member</p> : null}
+      <p className="details-p"><strong>Official name</strong>: {country.name.official}</p>
+      <p className="details-p"><strong>Area</strong>: {country.area} km2</p>
+      <p className="details-p"><strong>Capital</strong>: {country.capital}</p>
+      <p className="details-p"><strong>Continent</strong>: {country.continents}</p>
+      <p className="details-p"><strong>Languages</strong>:</p>
       {Object.entries(country.languages).map(([code, language]) => (
-        <li key={code}>{language}</li>
+        <li className="details-p list-languages" key={code}>{language}</li>
       ))}
+      <p className="details-p"><strong>Status</strong>: {country.tld.independent = true ? "Independent" : "Not independent"}</p>
       
     </div>) : <div>Page loading...</div>
   );
